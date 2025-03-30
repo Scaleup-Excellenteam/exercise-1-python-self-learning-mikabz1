3fe97f937fa6824c19d96d54343152550a6700f4
Michael Benzekri <110330086+mikabz1@users.noreply.github.com>
2025-03-30 21:55:35 +0300

    Update 5.4.communicating_vessels.py

--- src/5.4.communicating_vessels.py
+++ src/5.4.communicating_vessels.py
@@ -6,14 +6,14 @@ def communicating_vessels(*args:Iterable):
     """Interleave elements from multiple iterables into a list."""
     return list(chain.from_iterable(zip(*args)))
 
-def communicating_vessels_generator(*args:Iterable):
+def interleave(*args:Iterable):
     for elem in zip(*args):
         yield from elem
 
 
 if __name__ == '__main__':
     print(communicating_vessels('abc', [1, 2, 3], ('!', '@', '#')))
-    for i in communicating_vessels_generator('abc', [1, 2, 3], ('!', '@', '#')):
+    for i in interleave('abc', [1, 2, 3], ('!', '@', '#')):
         print(i , end=",")
 
 
