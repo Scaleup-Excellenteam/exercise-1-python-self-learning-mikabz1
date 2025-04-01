1f695c8115313ec36fc485041cce2757072d34a7
Michael Benzekri <110330086+mikabz1@users.noreply.github.com>
2025-04-01 14:52:56 +0300

    Update twice.py

--- src/twice.py
+++ src/twice.py
@@ -1,13 +1,28 @@
+"""
+This module defines a decorator `twice` that calls a function twice when it is decorated.
+"""
+
 from decorator import decorator
 
 @decorator
 def twice(func, *args, **kwargs):
+    """
+    A decorator that calls the decorated function twice.
+
+    :param func: The function to be decorated.
+    :param args: Arguments passed to the decorated function.
+    :param kwargs: Keyword arguments passed to the decorated function.
+    :return: The result of the second function call.
+    """
     func(*args, **kwargs)
     return func(*args, **kwargs)
 
 # Example usage
 @twice
 def say_hello():
+    """
+    Prints a hello message.
+    """
     print("Hello!")
 
 if __name__ == "__main__":
