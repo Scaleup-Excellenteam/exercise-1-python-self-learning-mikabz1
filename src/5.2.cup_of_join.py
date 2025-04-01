aac25c4a2db20e22cc61bcad4c151bc27e1a9a3e
Michael Benzekri <110330086+mikabz1@users.noreply.github.com>
2025-04-01 13:41:07 +0300

    Update 5.2.cup_of_join.py

--- src/5.2.cup_of_join.py
+++ src/5.2.cup_of_join.py
@@ -1,17 +1,20 @@
-def cup_of_join(*args , sep=""):
-    """
-       Combines multiple lists into one, inserting a separator between them. Defaults to `"-"` if no separator is provided.
-       Returns None if no lists are provided.
-       """
+"""This module provides a function to join multiple lists with a separator."""
 
-    if not args: return None
+def cup_of_join(*args, sep=""):
+    """
+    Combines multiple lists into one, inserting a separator between them.
+
+    Defaults to `"-"` if no separator is provided.
+    Returns None if no lists are provided.
+    """
+    if not args:
+        return None
 
     result = []
-    for i , lst in enumerate(args):
+    for i, lst in enumerate(args):
         result.extend(lst)
-        if i <= len(args) - 1:
-            if sep != "":
-                result.append(sep)
+        if i < len(args) - 1 and sep != "":
+            result.append(sep)
 
     return result
 
