f80dff9cd20d58007a5c03fc50adc4bdc7bac68a
Michael Benzekri <110330086+mikabz1@users.noreply.github.com>
2025-04-16 20:03:12 +0300

    Update 5.2.piece_of_cake.py
    adding if for the None

--- src/5.2.piece_of_cake.py
+++ src/5.2.piece_of_cake.py
@@ -1,20 +1,18 @@
-"""
-This module provides a function to calculate the weighted sum of values from a dictionary.
-It computes the sum based on specific keys and their values, excluding optional keys.
-"""
-
-
-def piece_of_cake(prices:dict , optionals = None , **kwargs ):
+def piece_of_cake(prices: dict, optionals=None, **kwargs):
     """
-       Calculates the weighted sum of values from `dictionary` based on `kwargs`, excluding keys in `optional`.
-       Each value in `kwargs` is multiplied by the corresponding value in `dictionary` and divided by 100.
-       Returns the total sum of the calculated values.
-       """
+    Calculates the weighted sum of values from `prices` based on `kwargs`,
+    excluding keys in `optionals`.
+    Each value in `kwargs` is multiplied by the corresponding value in `prices` and divided by 100.
+    Returns the total sum of the calculated values.
+    """
+    if optionals is None:
+        optionals = []
+
     try:
-        return sum(val / 100 * prices[name] for name , val in kwargs.items() if name not in optionals)
+        return sum(val / 100 * prices[name] for name, val in kwargs.items() if name not in optionals)
     except KeyError:
         print("KeyError")
         return None
 
 if __name__ == '__main__':
-    print(piece_of_cake({ 'milk': 8}, chocolate=200, milk=100))
+    print(piece_of_cake({'milk': 8}, chocolate=200, milk=100))  # Output: 8.0
