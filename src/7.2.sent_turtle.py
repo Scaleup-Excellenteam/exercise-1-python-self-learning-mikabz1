b97e8140819f83dda6d9bd6e0cb92b971d8994f2
Michael Benzekri <110330086+mikabz1@users.noreply.github.com>
2025-04-16 20:32:21 +0300

    Update 7.2.sent_turtle.py

--- src/7.2.sent_turtle.py
+++ src/7.2.sent_turtle.py
@@ -47,11 +47,6 @@ class PostOffice:
         return self.message_id
 
     def read_inbox(self, username , n = 0):
-        """
-           Retrieves up to N unread messages from the user's inbox and marks them as read.
-           If N is 0, retrieves all unread messages.
-           Raises KeyError if the username does not exist.
-        """
         try:
             user_box = self.boxes[user_name]
         except KeyError as exc:
